9c76ee03b0f1865f170605b925cadcc9dc48cf63
MakEva <sher.evikm@gmail.com>
2023-10-30 00:07:19 +0200

    new

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createGalleryMarkup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description} />
+  </a>
+</li>`
+}).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", createGalleryMarkup);
+
+galleryEl.addEventListener("click", onImageClick);
+ 
+function onImageClick(event) {
+  event.preventDefault();
+    const imageURL = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src=${imageURL}></img>`);
+  
+  instance.show();
+    
+document.addEventListener("keydown", onEscapeClose);
+
+  function onEscapeClose(event) {
+    if (event.code === "Escape") {
+      document.removeEventListener("keydown", onEscapeClose);
+      instance.close();
+    }
+  }
+}
+  
